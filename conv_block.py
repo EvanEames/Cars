dfd3ce64d52c1e1dba3d20a659b5a3985301bb06
Evan <38322201+EvanEames@users.noreply.github.com>
2019-07-16 12:27:41 +0200

    Update conv_block.py
    adding Scale after BatchNorm

--- conv_block.py
+++ conv_block.py
@@ -1,5 +1,7 @@
 from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+
 from keras.initializers import glorot_uniform
+from custom_layers.scale_layer import Scale
 
 def convolutional_block(X, f, filters, stage, block, s = 2):
     """
@@ -18,8 +20,10 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
     """
     
     # defining name basis
+    eps = 1.1e-5
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
+    scale_name_base = 'scale' + str(stage) + block + '_branch'
     
     # Retrieve Filters
     F1, F2, F3 = filters
@@ -27,25 +31,28 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
     # Save the input value
     X_shortcut = X
 
-
     ##### MAIN PATH #####
     # First component of main path 
-    X = Conv2D(F1, (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Conv2D(F1, (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '2a', use_bias=False, kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(epsilon = eps, axis = 3, name = bn_name_base + '2a')(X)
+    X = Scale(axis=3, name = scale_name_base + '2a')(X)
     X = Activation('relu')(X)
 
     # Second component of main path
-    X = Conv2D(F2, (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Conv2D(F2, (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', use_bias=False, kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(epsilon = eps, axis = 3, name = bn_name_base + '2b')(X)
+    X = Scale(axis=3, name = scale_name_base + '2b')(X)
     X = Activation('relu')(X)
 
     # Third component of main path
-    X = Conv2D(F3, (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
-    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+    X = Conv2D(F3, (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', use_bias=False, kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(epsilon = eps, axis = 3, name = bn_name_base + '2c')(X)
+    X = Scale(axis=3, name=scale_name_base + '2c')(X)
 
     ##### SHORTCUT PATH ####
-    X_shortcut = Conv2D(F3, (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '1', kernel_initializer = glorot_uniform(seed=0))(X_shortcut)
-    X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+    X_shortcut = Conv2D(F3, (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '1', use_bias=False, kernel_initializer = glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(epsilon = eps, axis = 3, name = bn_name_base + '1')(X_shortcut)
+    X_shortcut = Scale(axis=3, name=scale_name_base + '1')(X_shortcut)
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation
     X = Add()([X,X_shortcut])
